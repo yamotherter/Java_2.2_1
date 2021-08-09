df54ba476350c084473ce410fee2de8fecfb2fa7
Daria Peydus <dasha_vishnya@protonmail.com>
2021-08-10 00:09:06 +0300

    Добавлены комментарии о работе калькулятора

--- BonusMilesService.java
+++ BonusMilesService.java
@@ -1,4 +1,7 @@
 public class BonusMilesService {
+    /* Создаём метод calculate, который принимает стоимость билета,
+    производит рассчёт и возвращает количество бонусных миль
+     */
     public int calculate (int cost) {
         int mile_cost = 20;
         int miles = cost / mile_cost;
